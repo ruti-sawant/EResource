6fc2bf4e38df5a7687b45a3a52592bab5c409118
ruti-sawant <rutikish.sawant@gmail.com>
2021-11-15 18:11:39 +0530

    allowed all methods in access control as *

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ dotenv.config();
 function middleware(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "content-type");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
     next();
 }
